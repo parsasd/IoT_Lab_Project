b0e0499811f03646a6aa7207246123d450630f9b
parsasd <79263602+parsasd@users.noreply.github.com>
2025-05-06 16:40:36 +0330

    utils changed

--- web_app/email_utils.py
+++ web_app/email_utils.py
@@ -1,9 +1,18 @@
 from flask_mail import Message
 from flask import current_app
-from config import MAIL_USERNAME
-from app import mail
 
 def send_email(to, subject, body):
-    msg = Message(subject, sender=MAIL_USERNAME, recipients=[to])
+    """
+    Send an email using the Flask-Mail extension that's been
+    registered on the current_app.
+    """
+    # Grab the Mail instance from Flask's extensions registry
+    mail = current_app.extensions.get('mail')
+    if mail is None:
+        raise RuntimeError("Flask-Mail not initialized")
+
+    msg = Message(subject,
+                  sender=current_app.config['MAIL_USERNAME'],
+                  recipients=[to])
     msg.body = body
     mail.send(msg)
