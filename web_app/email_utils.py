1d3491f19f04d700f634646065f2dbeb5b31cc90
parsasd <79263602+parsasd@users.noreply.github.com>
2025-05-06 16:37:03 +0330

    utils

--- web_app/email_utils.py
+++ web_app/email_utils.py
@@ -1,7 +1,7 @@
 from flask_mail import Message
 from flask import current_app
 from config import MAIL_USERNAME
-from .app import mail
+from app import mail
 
 def send_email(to, subject, body):
     msg = Message(subject, sender=MAIL_USERNAME, recipients=[to])
